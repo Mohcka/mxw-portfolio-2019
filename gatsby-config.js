a5c238713187c69fe00a6f6dc0cc419fa34f41e0
Mohcka <bandagedwolf95@gmail.com>
2019-09-18 22:32:02 -0700

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,25 +4,25 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `MXW Portfolio`,
+    siteTitleAlt: `MXW Portfolio`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `MXW Portfolio - Mohcka/mxw-portfolio-2019`,
+        short_name: `Mohcka's Portfolio`,
+        description: `Check out my portfolio, yeah?`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
@@ -31,17 +31,17 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-netlify`
+  ]
+};
